95de3eb5aa82c7f712a53db3fdd0284efde00db3
VinZh22 <imthehellcat01@gmail.com>
2024-12-06 17:22:21 +0100

    corrected no distributed case

--- src/logistic_reg.py
+++ src/logistic_reg.py
@@ -36,6 +36,34 @@ def cost(theta: np.ndarray, X: np.ndarray, y: np.ndarray) -> float:
     z = X @ theta
     return -np.sum( y * np.log(sigmoid(z)) + (1 - y)*np.log(1 - sigmoid(z)) ) / X.shape[0]
 
+def costDecentralized(theta: np.ndarray, X: np.ndarray, y: np.ndarray, G : np.ndarray, D : np.ndarray, c : np.ndarray) -> float:
+    """    
+    Parameters
+    ----------
+    theta : np.ndarray
+        The parameters of the logit regression, of shape (Nb_models, n_features)
+    X : np.ndarray
+        The points, of shape (Nb_models, n_sample, n_features)
+    y : np.ndarray
+        The labels, of shape (Nb_models, n_sample, 1)
+
+    Returns
+    -------
+    float
+        The sum of the cost for each sample
+    """
+    terme = 0
+    Nb_model = X.shape[0]
+    for i in range(Nb_model):
+        for j in range(i+1, Nb_model):
+            terme += G[i,j] * np.linalg.norm(theta[i] - theta[j])**2
+    
+    terme2 = 0
+    for i in range(Nb_model):
+        terme2 += D[i] * c[i] * cost(theta[i], X[i], y[i])
+    
+    return 0.5 * terme + mu * terme2
+
 def compute_grad(theta: np.ndarray, X:np.ndarray, y:np.ndarray) -> np.ndarray:
     """Computes the gradient of the cost with respect to the parameters.
     
